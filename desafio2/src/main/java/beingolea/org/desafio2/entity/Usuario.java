90c14d3f4ae9ed30369c466e012fb045c94d4150
tapusudrema <129124089+tapusudrema@users.noreply.github.com>
2023-09-29 20:26:52 -0500

    Delete desafio2/src directory

--- desafio2/src/main/java/beingolea/org/desafio2/entity/Usuario.java
+++
@@ -1,6 +0,0 @@
-package beingolea.org.desafio2.entity;
-import beingolea.org.desafio2.service.SingledLinkedList;
-
-public class Usuario {
-    public static SingledLinkedList fila = new SingledLinkedList();
-}
